451dd3b477d5bdee2a334b5db2c4a67ff259de59
Oleh Papka <olegpapka2002@gmail.com>
2023-03-24 19:15:16 +0200

    Completed Task4

---
+++ Assets/Scripts/Player/AnimationController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+
+    private Animator _animator;
+    private Rigidbody2D _rigidbody;
+    private enum PlayerState { idle, running, jumping, falling};
+
+
+    public void UpdatePlayerState(float direction)
+    {
+        PlayerState state;
+        if (direction != 0f)
+            state = PlayerState.running;
+        else
+            state = PlayerState.idle;
+
+        if (_rigidbody.velocity.y > .1f)
+            state = PlayerState.jumping;
+        else if (_rigidbody.velocity.y < -.1f)
+            state = PlayerState.falling;
+        _animator.SetInteger("state", (int)state);
+    }
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody= GetComponent<Rigidbody2D>();
+    }
+
+    
+    
+}
---
+++ Assets/Scripts/Player/PlayerLife.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody= GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (IsFallenDown())
+        {
+            Die();
+            RestartLevel();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+            RestartLevel();
+        }
+    }
+
+    private void Die()
+    {
+        _rigidbody.bodyType = RigidbodyType2D.Static;
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private bool IsFallenDown()
+    {
+        float downLine = -15;
+        return _rigidbody.position.y < downLine ? true : false;
+    }
+}
